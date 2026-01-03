f4151b17a86a4900049b9df7bb7425a26ddc237a
dorkem <20233144@gs.cwnu.ac.kr>
2026-01-03 15:39:41 +0900

    ⚙️ file setting

---
+++ TOBY_EXAMPLE/toby/src/test/java/com/toby/app/TobyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toby.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
