43a8887481e0d23119e21b6d2fea743c0dc0ec76
dorkem <20233144@gs.cwnu.ac.kr>
2026-01-04 08:53:50 +0900

    :recycle: 제네릭스를 통한 템플릿/콜백 설계

--- TOBY_EXAMPLE/toby/src/main/java/calculator/template/Calculator.java
+++ TOBY_EXAMPLE/toby/src/main/java/calculator/template/Calculator.java
@@ -5,12 +5,12 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Calculator {
-	public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+	public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException {
 		BufferedReader br = null;
 
 		try {
 			br = new BufferedReader(new FileReader(filepath));
-			Integer res = initVal;
+			T res = initVal;
 			String line = null;
 			while ((line = br.readLine()) != null) {
 				res = callback.doSomethingWithLine(line, res);
@@ -28,8 +28,9 @@ public class Calculator {
 	}
 
 	public Integer calcSum(String filepath) throws IOException {
-		LineCallback sumCallback =
-			new LineCallback() {
+		LineCallback<Integer> sumCallback =
+			new LineCallback<Integer>() {
+			@Override
 				public Integer doSomethingWithLine(String line, Integer value) {
 					return value + Integer.parseInt(line);
 				}
@@ -38,12 +39,22 @@ public class Calculator {
 	}
 
 	public Integer calcMultiply(String filepath) throws IOException {
-		LineCallback sumCallback =
-			new LineCallback() {
+		LineCallback<Integer> multiplyCallback =
+			new LineCallback<Integer>() {
 				public Integer doSomethingWithLine(String line, Integer value) {
 					return value * Integer.parseInt(line);
 				}
 			};
-		return lineReadTemplate(filepath, sumCallback, 1);
+		return lineReadTemplate(filepath, multiplyCallback, 1);
+	}
+
+	public String concatenate(String filepath) throws IOException {
+		LineCallback<String> concatenateCallback =
+			new LineCallback<String>() {
+				public String doSomethingWithLine(String line, String value) {
+					return value + line;
+				}
+			};
+		return lineReadTemplate(filepath, concatenateCallback, "");
 	}
 }
