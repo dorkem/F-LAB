0f0e60eff930b3d2dfb4ac63cf2d8d3ed589a1d2
dorkem <20233144@gs.cwnu.ac.kr>
2026-01-04 02:06:58 +0900

    :recycle: 덧셈, 곱셈에 대한 콜백/템플릿 설계

---
+++ TOBY_EXAMPLE/toby/src/test/java/calculator/template/CalculatorTest.java
@@ -0,0 +1,30 @@
+package calculator.template;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+	Calculator calculator;
+	String numFilepath;
+
+	@BeforeEach
+	public void setUp() {
+		this.calculator = new Calculator();
+		this.numFilepath = getClass().getResource("/numbers.txt").getPath();
+	}
+
+	@Test
+	public void sumOfNumbers() throws IOException {
+		assertThat(calculator.calcSum(this.numFilepath), is(10));
+	}
+
+	@Test
+	public void multiplyOfNumbers() throws IOException {
+		assertThat(calculator.calcMultiply(this.numFilepath), is(24));
+	}
+}
