e1faf57c640d43df353e8a00275d1bd247f591e0
dorkem <20233144@gs.cwnu.ac.kr>
2026-01-03 17:57:14 +0900

    :recycle: 공통로직 메서드 분리

--- TOBY_EXAMPLE/toby/src/main/java/Chapter03/user/dao/UserDao.java
+++ TOBY_EXAMPLE/toby/src/main/java/Chapter03/user/dao/UserDao.java
@@ -59,7 +59,7 @@ public class UserDao {
 
 		try {
 			c = dataSource.getConnection();
-			ps = c.prepareStatement("delete from users");
+			ps = makeStatement(c);
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			throw e;
@@ -114,5 +114,11 @@ public class UserDao {
 				}
 			}
 		}
+	}
+
+	private PreparedStatement makeStatement(Connection c) throws SQLException {
+		PreparedStatement ps;
+		ps = c.prepareStatement("delete from users");
+		return ps;
 	}
 }
