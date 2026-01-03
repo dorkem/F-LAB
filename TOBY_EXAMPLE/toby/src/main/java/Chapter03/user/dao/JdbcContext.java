4986497e6a0f4c620906184c30132072cbad291d
dorkem <20233144@gs.cwnu.ac.kr>
2026-01-03 18:52:08 +0900

    :recycle: di 방식 수정

---
+++ TOBY_EXAMPLE/toby/src/main/java/Chapter03/user/dao/JdbcContext.java
@@ -0,0 +1,31 @@
+package Chapter03.user.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+public class JdbcContext {
+	private DataSource dataSource;
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public void workWithStatmentStrategy(StatementStrategy stmt) throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+
+		try {
+			c = dataSource.getConnection();
+			ps = stmt.makePrepareStatement(c);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) { try { ps.close(); } catch (SQLException e) {}}
+			if (c != null) { try { c.close(); } catch (SQLException e) {}}
+		}
+	}
+}
--- TOBY_EXAMPLE/toby/src/main/java/Chapter03/user/dao/UserDao.java
+++ TOBY_EXAMPLE/toby/src/main/java/Chapter03/user/dao/UserDao.java
@@ -11,18 +11,23 @@ import Chapter03.user.domain.User;
 
 public class UserDao {
 	private DataSource dataSource;
+	private JdbcContext jdbcContext;
 
 	public void setDataSource(DataSource dataSource) {
+		this.jdbcContext = new JdbcContext();
+
+		jdbcContext.setDataSource(dataSource);
+
 		this.dataSource = dataSource;
 	}
 
 	public void add(final User user) throws SQLException {
-		jdbcContextWithStatmentStrategy(
+		this.jdbcContext.workWithStatmentStrategy(
 			new StatementStrategy() {
 				public PreparedStatement makePrepareStatement(Connection c) throws SQLException {
 					PreparedStatement ps =
 						c.prepareStatement(
-						"insert into users(id, name, password) values(?,?,?)");
+							"insert into users(id, name, password) values(?,?,?)");
 					ps.setString(1, user.getId());
 					ps.setString(2, user.getName());
 					ps.setString(3, user.getPassword());
@@ -33,30 +38,30 @@ public class UserDao {
 		);
 	}
 
-	public User get(String id) throws SQLException {
-		Connection c = dataSource.getConnection();
-
-		PreparedStatement ps = c.prepareStatement(
-			"select * from users where id = ?"
-		);
-		ps.setString(1, id);
-
-		ResultSet rs = ps.executeQuery();
-		rs.next();
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
-
-		rs.close();
-		ps.close();
-		c.close();
-
-		return user;
-	}
+	// public User get(String id) throws SQLException {
+	// 	Connection c = dataSource.getConnection();
+	//
+	// 	PreparedStatement ps = c.prepareStatement(
+	// 		"select * from users where id = ?"
+	// 	);
+	// 	ps.setString(1, id);
+	//
+	// 	ResultSet rs = ps.executeQuery();
+	// 	rs.next();
+	// 	User user = new User();
+	// 	user.setId(rs.getString("id"));
+	// 	user.setName(rs.getString("name"));
+	// 	user.setPassword(rs.getString("password"));
+	//
+	// 	rs.close();
+	// 	ps.close();
+	// 	c.close();
+	//
+	// 	return user;
+	// }
 
 	public void deleteAll() throws SQLException {
-		jdbcContextWithStatmentStrategy(
+		this.jdbcContext.workWithStatmentStrategy(
 			new StatementStrategy() {
 				public PreparedStatement makePrepareStatement(Connection c) throws SQLException {
 					return c.prepareStatement("delete from users");
@@ -65,56 +70,40 @@ public class UserDao {
 		);
 	}
 
-	public int getCount() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-
-		try {
-			c = dataSource.getConnection();
-
-			ps = c.prepareStatement("select count(*) from users");
-
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
-	}
-
-	public void jdbcContextWithStatmentStrategy(StatementStrategy stmt) throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-
-		try {
-			c = dataSource.getConnection();
-			ps = stmt.makePrepareStatement(c);
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (ps != null) { try { ps.close(); } catch (SQLException e) {}}
-			if (c != null) { try { c.close(); } catch (SQLException e) {}}
-		}
-	}
+	// public int getCount() throws SQLException {
+	// 	Connection c = null;
+	// 	PreparedStatement ps = null;
+	// 	ResultSet rs = null;
+	//
+	// 	try {
+	// 		c = dataSource.getConnection();
+	//
+	// 		ps = c.prepareStatement("select count(*) from users");
+	//
+	// 		rs = ps.executeQuery();
+	// 		rs.next();
+	// 		return rs.getInt(1);
+	// 	} catch (SQLException e) {
+	// 		throw e;
+	// 	} finally {
+	// 		if (rs != null) {
+	// 			try {
+	// 				rs.close();
+	// 			} catch (SQLException e) {
+	// 			}
+	// 		}
+	// 		if (ps != null) {
+	// 			try {
+	// 				ps.close();
+	// 			} catch (SQLException e) {
+	// 			}
+	// 		}
+	// 		if (c != null) {
+	// 			try {
+	// 				c.close();
+	// 			} catch (SQLException e) {
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
